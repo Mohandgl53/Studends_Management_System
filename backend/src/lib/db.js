f2da2fc602b3730746baccf3d7170bfb563c4583
Mohan Kumar <tomhollderdgl53@gmail.com>
2026-06-27 23:56:17 +0530

    connected the mongoDb

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,2 +1,14 @@
 import mongoose from "mongoose";
  
+export async function connectDB() {
+    const mongoUri = process.env.MONGO_URI;
+    if(!mongoUri){
+        console.log("MONGO_URI is Required");
+        return;
+    }
+    try{
+        await mongoose.connect(mongoUri);
+    }catch(error){
+        console.error("MongoDB connection error:", error.message);
+    }
+}
